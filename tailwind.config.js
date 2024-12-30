e667ed3520c0e138eff30d959ef052c8944eb172
Irbaz-Patel <125855032+Irbaz-Patel@users.noreply.github.com>
2024-12-30 11:18:24 +0530

    ColorUpdated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,43 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/preline/dist/*.js" 
+//   ],
+//   theme: {
+//     extend: {
+//       screens: {
+//         'media424px': '424px',
+//         'media375': '375px',
+//         'media320': '320px'
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('preline/plugin'),
+//   ],
+// }
+
 /** @type {import('tailwindcss').Config} */
+import prelinePlugin from 'preline/plugin';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/preline/dist/*.js" 
+    "./node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {
       screens: {
         'media424px': '424px',
         'media375': '375px',
-        'media320': '320px'
+        'media320': '320px',
       },
     },
   },
   plugins: [
-    require('preline/plugin'),
+    prelinePlugin,
   ],
-}
+};
